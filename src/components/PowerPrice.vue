c75a1a1cfc67cc4ee1f1d2eae8424e4ccb2c16b7
aleksanderboe <Aleksanderbo10@gmail.com>
2025-01-14 22:21:48 +0100

    fix color mismatch

--- src/components/PowerPrice.vue
+++ src/components/PowerPrice.vue
@@ -33,8 +33,8 @@ const chartData = ref({
     {
       label: "NOK per kWh",
       data: [],
-      borderColor: "rgba(75, 192, 192, 1)",
-      backgroundColor: "rgba(75, 192, 192, 0.2)",
+      borderColor: "rgba(255, 206, 86, 1)",
+      backgroundColor: "rgba(255, 206, 86, 0.2)",
       fill: true,
       tension: 0.4,
     },
